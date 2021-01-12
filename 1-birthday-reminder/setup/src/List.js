50cbbbc207789cd209a255b9e38c2823edd2109e
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-11 19:39:47 -0600

    add list component

--- 1-birthday-reminder/setup/src/List.js
+++ 1-birthday-reminder/setup/src/List.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const List = () => {
+const List = (data) => {
   return (
-    <>
-      <h2>list component</h2>
-    </>
+    <SContainer>
+      <SImage src={data.image} />
+      <STextContainer>
+        <h2>{data.name}</h2>
+        <h3>{data.age}</h3>
+      </STextContainer>
+    </SContainer>
   );
 };
 
 export default List;
+
+const SContainer = styled.div`
+  display: flex;
+`;
+
+const STextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const SImage = styled.img`
+  width: 80px;
+  height: 80px;
+  object-fit: cover;
+  border-radius: 50%;
+`;
