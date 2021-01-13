7868e247b3266827dbbd0c377ec61822847f6b21
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-12 20:07:00 -0600

    add categories

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ items }) => {
+  const [categories, setCategories] = React.useState([]);
+
+  React.useEffect(() => {
+    const categories = items.map((item) => item.category);
+    const uniqueCategories = Array.from(new Set(categories));
+    setCategories(uniqueCategories);
+  }, [items]);
+
+  return (
+    <SCategoriesContainer>
+      {categories.length > 0 &&
+        categories.map((category) => {
+          return <SCategory key={category}>{category}</SCategory>;
+        })}
+    </SCategoriesContainer>
+  );
 };
 
 export default Categories;
+
+const SCategoriesContainer = styled.ul`
+  display: flex;
+`;
+
+const SCategory = styled.li``;
