dd982fe5974b9c81e1393c6e99d79b49ce1759f3
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-12 19:41:05 -0600

    add q comp

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,33 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import styled from 'styled-components';
+
+const q = {
+  id: 1,
+  title: 'Do I have to allow the use of cookies?',
+  info:
+    'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+};
+
 const Question = () => {
-  return <h2>question component</h2>;
+  const [show, setShow] = React.useState(false);
+  const { id, title, info } = q;
+
+  return (
+    <SQuestionContainer>
+      <h4>{title}</h4>
+      {show && <p>{info}</p>}
+      {show ? <AiOutlineMinus /> : <AiOutlinePlus />}
+    </SQuestionContainer>
+  );
 };
 
 export default Question;
+
+const SQuestionContainer = styled.section`
+  display: flex;
+  box-shadow: 2px 2px 10px grey;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+`;
