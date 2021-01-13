7c60f94238ee88e9fc62ef60ece7f6f296550d56
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-12 19:16:45 -0600

    add review component

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -1,9 +1,44 @@
 import React, { useState } from 'react';
-import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import styled from 'styled-components';
+
+const whoever = {
+  id: 4,
+  name: 'bill anderson',
+  job: 'the boss',
+  image:
+    'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  text:
+    'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+};
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const { image, name, job, text } = whoever;
+  return (
+    <SContainer>
+      <SImage src={image} />
+      <h2>{name}</h2>
+      <h4>{job}</h4>
+      <p>{text}</p>
+    </SContainer>
+  );
 };
 
 export default Review;
+
+const SContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  width: 70vw;
+  padding: 20px;
+`;
+
+const SImage = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 50%;
+`;
