0e9a16f81497b4af6bfcd94bbc7137c8cfba9ef4
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-12 19:25:36 -0600

    setup useEffect and setIdx and state within app

--- 3-reviews/setup/src/App.js
+++ 3-reviews/setup/src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import people from './data';
 import Review from './Review';
 function App() {
-  return <Review />;
+  const [currentPersonIdx, setCurrentPersonIdx] = React.useState(0);
+  const [peopleData, _setPeopleData] = React.useState(people);
+  const [person, setPerson] = React.useState(people[0]);
+
+  React.useEffect(() => {
+    setPerson(peopleData[currentPersonIdx]);
+  }, [currentPersonIdx]);
+
+  const setIdx = () => {
+    if (currentPersonIdx === peopleData.length - 1) {
+      setCurrentPersonIdx(0);
+    } else {
+      setCurrentPersonIdx((i) => i + 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>Our Reviews</h1>
+    </>
+  );
 }
 
 export default App;
