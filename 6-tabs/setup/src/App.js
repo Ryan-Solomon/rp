cd5c5adb63c8984557f0ab2e6373d3e7bbee0311
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-14 17:46:26 -0600

    fetch user info

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -1,10 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = 'https://course-api.com/react-tabs-project';
 function App() {
-  return <h2>tabs project setup</h2>
+  const [status, setStatus] = React.useState('idle');
+  const [info, setInfo] = React.useState([]);
+
+  React.useEffect(() => {
+    const getData = async () => {
+      setStatus('pending');
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setInfo(data);
+        setStatus('fulfilled');
+      } catch (e) {
+        console.error(e.message);
+      }
+    };
+    getData();
+  }, []);
+
+  console.log(info);
+
+  return <h2>tabs project setup</h2>;
 }
 
-export default App
+export default App;
