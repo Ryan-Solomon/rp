51bb4b8fd5d6410f97750f49320ba0d5ffa4a1f3
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-13 18:16:27 -0600

    style menuitem

--- 5-menu/setup/src/MenuItem.js
+++ 5-menu/setup/src/MenuItem.js
@@ -3,11 +3,23 @@ import styled from 'styled-components';
 
 export const MenuItem = ({ item }) => {
   const { title, category, price, img, desc } = item;
-  return <h1>{title}</h1>;
+  return (
+    <SContainer>
+      <SImage src={img} />
+      <SContentContainer>
+        <STitleAndPriceContainer>
+          <STitle>{title}</STitle>
+          <SPrice>{price}</SPrice>
+        </STitleAndPriceContainer>
+        <SInfo>{desc}</SInfo>
+      </SContentContainer>
+    </SContainer>
+  );
 };
 
 const SContainer = styled.section`
   display: flex;
+  margin: 2rem;
 `;
 
 const SImage = styled.img`
@@ -19,15 +31,25 @@ const SImage = styled.img`
 const SContentContainer = styled.section`
   display: flex;
   flex-direction: column;
+  padding: 1rem;
 `;
 
 const STitleAndPriceContainer = styled.div`
   display: flex;
-  flex-direction: space-between;
+  justify-content: space-between;
+  width: 100%;
+  border-bottom: 1px solid black;
+  margin-bottom: 1rem;
 `;
 
-const STitle = styled.h2``;
+const STitle = styled.h2`
+  font-size: 1rem;
+`;
 
-const SPrice = styled.h2``;
+const SPrice = styled.h2`
+  font-size: 1rem;
+`;
 
-const SInfo = styled.p``;
+const SInfo = styled.p`
+  font-size: 1rem;
+`;
