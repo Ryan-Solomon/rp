3a46f8386a790b9b7e9b71bd20e863eeeea3c2fc
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-15 20:08:22 -0600

    add gbud and some style

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [list, setList] = useState([]);
+
+  return (
+    <SAppContainer>
+      <SGroceryContainer>
+        <h1>Grocery Bud</h1>
+        <label htmlFor='add-item'>Add Item</label>
+        <input type='text' name='add-item' id='add-item' />
+      </SGroceryContainer>
+    </SAppContainer>
+  );
 }
 
-export default App
+export default App;
+
+const SAppContainer = styled.div`
+  display: grid;
+  place-items: center;
+  background: #333;
+  height: 100vh;
+`;
+
+const SGroceryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 12rem;
+  color: white;
+  background: #222;
+  padding: 2rem;
+`;
