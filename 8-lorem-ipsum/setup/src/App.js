1220dec679b85e2a685876c865e24a76c0db9400
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-15 19:58:29 -0600

    add ipsum

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -4,22 +4,40 @@ import styled from 'styled-components';
 function App() {
   const [numberOfParagraphs, setNumberOfParagraphs] = useState(0);
   const [paragraphs, setParagraphs] = useState([]);
+  const [shouldGenerate, setShouldGenerate] = useState(false);
 
   useEffect(() => {
     if (numberOfParagraphs === 0) return;
+    if (!shouldGenerate) return;
     const getIpsum = async () => {
-      const res = await fetch(
-        `https://baconipsum.com/api/?type=meat-and-filler&paras=${numberOfParagraphs}&format=text`
-      );
-      const data = await res.json();
-      setParagraphs(data);
+      try {
+        const res = await fetch(
+          `https://baconipsum.com/api/?type=meat-and-filler&paras=${numberOfParagraphs}&format=text`
+        );
+
+        const data = await res.json();
+        setParagraphs(data);
+        setShouldGenerate(false);
+      } catch (e) {
+        console.log(e.message);
+      }
     };
     getIpsum();
-  }, [numberOfParagraphs]);
+  }, [numberOfParagraphs, shouldGenerate]);
+
+  console.log(paragraphs);
 
   return (
     <SContainer>
       <h1>Tired of boring ipsum?</h1>
+      <label htmlFor='count'>Paragraphs: </label>
+      <input
+        type='number'
+        id='count'
+        value={numberOfParagraphs}
+        onChange={(e) => setNumberOfParagraphs(e.target.value)}
+      />
+      <button onClick={() => setShouldGenerate(true)}>Generate</button>
     </SContainer>
   );
 }
