3e3655789a7e180be1b39c1666131034efc7246b
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-15 19:44:52 -0600

    fetch ipsum

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState, useEffect } from 'react';
+
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+  const [numberOfParagraphs, setNumberOfParagraphs] = useState(0);
+  const [paragraphs, setParagraphs] = useState([]);
+
+  useEffect(() => {
+    if (numberOfParagraphs === 0) return;
+    const getIpsum = async () => {
+      const res = await fetch(
+        `https://baconipsum.com/api/?type=meat-and-filler&paras=${numberOfParagraphs}&format=text`
+      );
+      const data = await res.json();
+      setParagraphs(data);
+    };
+    getIpsum();
+  }, [numberOfParagraphs]);
+
+  return <h2>lorem ipsum project setup</h2>;
 }
 
 export default App;
