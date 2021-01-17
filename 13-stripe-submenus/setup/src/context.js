db50629762557e8a6fa2faaf5e1116de93a80493
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-17 17:24:44 -0600

    setup app context

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,16 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [links, setlinks] = useState(sublinks);
+
+  const appData = { links };
+
+  return <AppContext.Provider value={appData}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
