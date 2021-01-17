c66597c93bfa0ab153d49aaf34e32f01607ea6b5
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-16 19:44:04 -0600

    style modal button

--- 12-sidebar-modal/setup/src/Modal.js
+++ 12-sidebar-modal/setup/src/Modal.js
@@ -1,7 +1,47 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-const Modal = () => {
-  return <h2>Modal</h2>
-}
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import styled from 'styled-components';
 
-export default Modal
+const Modal = () => {
+  return (
+    <SModalContainer>
+      <SModalButton>
+        <SModalText>Show Modal</SModalText>
+      </SModalButton>
+    </SModalContainer>
+  );
+};
+
+export default Modal;
+
+const SModalContainer = styled.div`
+  height: 90vh;
+  width: 90vw;
+  margin: auto;
+  display: grid;
+  place-items: center;
+`;
+
+const SModalButton = styled.button`
+  background: #333;
+  border-radius: 10px;
+  box-shadow: 2px 2px 10px #a3a3a3;
+  display: grid;
+  place-items: center;
+  padding: 0 12px;
+  color: white;
+  transition: all 0.2s ease;
+  border: none;
+  outline: none;
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(0.96);
+    background: white;
+    color: #333;
+  }
+`;
+
+const SModalText = styled.h3`
+  font-size: 1rem;
+`;
