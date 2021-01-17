67a94a7af97bd0ccd291f621f4168aac495a7f17
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-16 18:14:02 -0600

    set window width

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -5,8 +5,21 @@ import logo from './logo.svg';
 import styled from 'styled-components';
 
 const Navbar = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const setWidth = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', setWidth);
+
+    return () => window.removeEventListener('resize', setWidth);
+  }, []);
+
   return (
     <SNavContainer>
+      <h2>{windowWidth}</h2>
       <SHeader>Coding Addict</SHeader>
       <SListContainer>
         {links.map(({ text }) => {
@@ -27,14 +40,20 @@ export default Navbar;
 const SNavContainer = styled.nav`
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  padding: 0.5rem 1rem;
+  padding-top: 1rem;
 `;
 
 const SHeader = styled.h1`
-  font-size: 2rem;
+  font-size: 1.4rem;
 `;
 
 const SListContainer = styled.ul`
   display: flex;
+  align-items: center;
 `;
 
-const SListItem = styled.li``;
+const SListItem = styled.li`
+  margin: 0px 0.5rem;
+`;
