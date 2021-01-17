bd9fa926ca3c20d7ff3b9108f7b1d69b82d3a313
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-16 18:03:12 -0600

    add nav

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,40 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
+import styled from 'styled-components';
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  return (
+    <SNavContainer>
+      <SHeader>Coding Addict</SHeader>
+      <SListContainer>
+        {links.map(({ text }) => {
+          return <SListItem>{text}</SListItem>;
+        })}
+      </SListContainer>
+      <SListContainer>
+        {social.map(({ icon }) => {
+          return <SListItem>{icon}</SListItem>;
+        })}
+      </SListContainer>
+    </SNavContainer>
+  );
+};
 
-export default Navbar
+export default Navbar;
+
+const SNavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+`;
+
+const SHeader = styled.h1`
+  font-size: 2rem;
+`;
+
+const SListContainer = styled.ul`
+  display: flex;
+`;
+
+const SListItem = styled.li``;
