cf11d899e4e762c34377b2912b6ff985bf1ebda2
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-17 17:59:49 -0600

    add link component

--- 13-stripe-submenus/setup/src/Link.js
+++ 13-stripe-submenus/setup/src/Link.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const Link = () => {
-  return <h1>link</h1>;
+export const Link = ({ link }) => {
+  const { label, icon, url } = link;
+  return (
+    <LinkContainer href={url}>
+      {icon}
+      <LabelText>{label}</LabelText>
+    </LinkContainer>
+  );
 };
+
+const LinkContainer = styled.a`
+  display: flex;
+  padding: 10px;
+  text-decoration: none;
+  color: white;
+`;
+
+const LabelText = styled.h3`
+  color: white;
+  font-size: 16px;
+  margin-left: 10px;
+`;
