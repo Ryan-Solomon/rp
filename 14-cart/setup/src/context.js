ab9c722a3d54eb70d27d9f5dd945f9b7bceee0ca
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-17 19:57:06 -0600

    fetch data

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -1,13 +1,29 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react'
-import cartItems from './data'
-import reducer from './reducer'
+import React, { useState, useContext, useReducer, useEffect } from 'react';
+import cartItems from './data';
+import reducer from './reducer';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-useReducer-cart-project'
-const AppContext = React.createContext()
+const url = 'https://course-api.com/react-useReducer-cart-project';
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
+  const [cart, setCart] = useState(cartItems);
+  const [status, setStatus] = useState('idle');
+
+  useEffect(() => {
+    const getItems = async () => {
+      setStatus('loading');
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+        setStatus('fulfilled');
+      } catch (e) {
+        setStatus('error');
+      }
+    };
+    getItems();
+  }, []);
 
   return (
     <AppContext.Provider
@@ -17,11 +33,11 @@ const AppProvider = ({ children }) => {
     >
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
