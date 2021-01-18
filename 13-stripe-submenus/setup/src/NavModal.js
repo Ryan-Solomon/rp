b87571f0a0ceaec47752284cf422eab84a9be689
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-17 18:23:31 -0600

    add modal x

--- 13-stripe-submenus/setup/src/NavModal.js
+++ 13-stripe-submenus/setup/src/NavModal.js
@@ -2,7 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const NavModal = () => {
-  return <h1>Nav Modal</h1>;
+  return (
+    <ModalContainer>
+      <ModalX>X</ModalX>
+    </ModalContainer>
+  );
 };
 
-const ModalContainer = styled.div``;
+const ModalContainer = styled.div`
+  background: #fff;
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  height: 90vh;
+  width: 90vw;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ModalX = styled.button`
+  background: none;
+  border: none;
+  outline: none;
+  font-size: 24px;
+  font-weight: bold;
+  align-self: flex-end;
+  margin-right: 30px;
+  margin-top: 20px;
+`;
